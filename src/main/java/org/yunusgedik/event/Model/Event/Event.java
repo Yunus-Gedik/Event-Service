f89a0ce889703c98d023d74373f547380a757002
Yunus Gedik <yunus-gedik@outlook.com>
2025-07-27 18:58:40 +0300

    feat: event and event category entity models

---
+++ src/main/java/org/yunusgedik/event/Model/Event/Event.java
@@ -0,0 +1,34 @@
+package org.yunusgedik.event.Model.Event;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.yunusgedik.event.Model.EventCategory.EventCategory;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "event")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(nullable = false, name = "categoryId")
+    private EventCategory category;
+
+    private String title;
+    private String description;
+    private String location;
+    private LocalDateTime eventDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastUpdatedAt;
+    private int capacity;
+    private double price;
+    private boolean isActive;
+}
---
+++ src/main/java/org/yunusgedik/event/Model/EventCategory/EventCategory.java
@@ -0,0 +1,27 @@
+package org.yunusgedik.event.Model.EventCategory;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.yunusgedik.event.Model.Event.Event;
+
+import java.util.Set;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@Table(name = "eventCategory")
+public class EventCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Event> events;
+}
