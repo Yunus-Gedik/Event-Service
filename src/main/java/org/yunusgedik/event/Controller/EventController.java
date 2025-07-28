91ddf263c7a0c2e1178dd931a860a82a2d767b18
Yunus Gedik <yunus-gedik@outlook.com>
2025-07-28 19:15:54 +0300

    feat: event controller

---
+++ src/main/java/org/yunusgedik/event/Controller/EventController.java
@@ -0,0 +1,48 @@
+package org.yunusgedik.event.Controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.yunusgedik.event.Model.Event.Event;
+import org.yunusgedik.event.Model.Event.EventDTO;
+import org.yunusgedik.event.Service.EventService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/event")
+public class EventController {
+    private final EventService eventService;
+
+    EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("")
+    public Event get(@RequestParam(name = "id", required = true) Long id){
+        return this.eventService.get(id);
+    }
+
+    @GetMapping("/all")
+    public List<Event> getAll(){
+        return this.eventService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Event getByPathVariable(@PathVariable(name = "id" ,required = true) Long id){
+        return this.eventService.get(id);
+    }
+
+    @PostMapping("/new")
+    public Event create(@RequestBody EventDTO eventDTO){
+        return this.eventService.create(eventDTO);
+    }
+
+    @PatchMapping("/update")
+    public Event update(@RequestBody EventDTO eventDTO){
+        return this.eventService.update(eventDTO);
+    }
+
+    @DeleteMapping("")
+    public Event delete(@RequestParam(name = "id", required = true) Long id){
+        this.eventService.delete(id);
+    }
+}
