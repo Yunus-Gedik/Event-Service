bbb4175338b6f95b76db16e50c252ee2ad0e6747
Yunus Gedik <yunus-gedik@outlook.com>
2025-07-28 19:16:55 +0300

    feat: event category service

---
+++ src/main/java/org/yunusgedik/event/Service/EventCategoryService.java
@@ -0,0 +1,52 @@
+package org.yunusgedik.event.Service;
+
+import org.springframework.stereotype.Service;
+import org.yunusgedik.event.Model.EventCategory.EventCategory;
+import org.yunusgedik.event.Model.EventCategory.EventCategoryDTO;
+import org.yunusgedik.event.Repository.EventCategoryRepository;
+
+import java.util.List;
+
+@Service
+public class EventCategoryService {
+
+    private final EventCategoryRepository repository;
+
+    public EventCategoryService(EventCategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    public EventCategory get(Long id) {
+        return this.repository.findById(id).orElse(null);
+    }
+
+    public List<EventCategory> getAll() {
+        return this.repository.findAll();
+    }
+
+    public EventCategory create(EventCategoryDTO categoryDTO) {
+        EventCategory eventCategory = new EventCategory();
+        eventCategory.setName(categoryDTO.getName());
+        if (categoryDTO.getId() != null &&
+            !repository.existsById(categoryDTO.getId()) &&
+            categoryDTO.getId() > 0) {
+            return this.repository.save(eventCategory);
+        }
+        return null;
+    }
+
+    public EventCategory update(EventCategoryDTO categoryDTO) {
+        EventCategory eventCategory = this.get(categoryDTO.getId());
+        eventCategory.setName(categoryDTO.getName());
+        return this.repository.save(eventCategory);
+    }
+
+    public EventCategory delete(Long id) {
+        EventCategory eventCategory = this.get(id);
+        if (eventCategory != null) {
+            this.repository.delete(eventCategory);
+            return eventCategory;
+        }
+        return null;
+    }
+}
